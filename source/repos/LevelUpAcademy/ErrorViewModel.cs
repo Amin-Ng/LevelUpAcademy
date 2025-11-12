c9abdf1491ca2726e358d7eaf21e51ce2a9f8a70
Amin-Ng <a.pemboura@itsolivetti.it>
2025-11-12 12:48:43 -0800

    Delete source/repos/LevelUpAcademy directory

--- source/repos/LevelUpAcademy/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace LevelUpAcademy.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
